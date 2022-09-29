a176bab0e89a3ae0d12274b8e435ecf2786d60bf
Maruf Mia <mmaruf1137@gmail.com>
2022-09-29 11:08:36 +0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const sidebarOpenBtn = document.querySelector('.sidebar-open-btn');
+const dashboard = document.querySelector('.dashboard');
+
+sidebarOpenBtn.addEventListener('click', () => {
+	dashboard.classList.toggle('show-sidebar');
+});
+
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 2,
+	spaceBetween: 20,
+	loop: true,
+});
+
+const swiper2 = new Swiper('.swiper-2', {
+	slidesPerView: 4,
+	// spaceBetween: 20,
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		// el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// },
+});
