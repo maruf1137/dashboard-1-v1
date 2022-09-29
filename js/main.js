0ed04e6cc09438567f1a3c8131df59df72ae6284
Maruf Mia <mmaruf1137@gmail.com>
2022-09-29 12:09:40 +0600

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const sidebarOpenBtn = document.querySelector('.sidebar-open-btn');
+const sidebarOpenBtn = document.querySelector('.btn-plus');
 const dashboard = document.querySelector('.dashboard');
 
 sidebarOpenBtn.addEventListener('click', () => {
